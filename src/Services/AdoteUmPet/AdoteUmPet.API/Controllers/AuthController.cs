c50d9fdda44b54ac748b25190b8c6cdd32ac2375
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-09-12 20:31:49 -0300

    Criação do Controller de authentication e primeiros métodos para cadastro e login, além de input models.

---
+++ src/Services/AdoteUmPet/AdoteUmPet.API/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using AdoteUmPet.Application.Models.InputModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdoteUmPet.API.Controllers
+{
+    [Route("/api/v1/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        /// <summary>
+        /// Registra um novo usuário
+        /// </summary>
+        /// <param name="registerUser"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterUserInputModel registerUser)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.Values.SelectMany(p => p.Errors));
+
+            IdentityUser user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true,
+            };
+
+            IdentityResult createResult = await _userManager.CreateAsync(user, registerUser.Password);
+
+            if (!createResult.Succeeded)
+                return BadRequest(createResult.Errors);
+
+            await _signInManager.SignInAsync(user, false);
+
+            return Ok();
+        }   
+    }
+}
---
+++ src/Services/AdoteUmPet/AdoteUmPet.Application/Models/InputModels/RegisterUserInputModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoteUmPet.Application.Models.InputModels
+{
+    public class RegisterUserInputModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
