6dc4b6dd3fc1dbc3de6ea3bfa550569ea063ed92
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-09-12 16:04:35 -0300

    Implementado o EfCore e primeiro contexto à aplicação.

--- src/Services/AdoteUmPet/AdoteUmPet.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AdoteUmPet.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Services/AdoteUmPet/AdoteUmPet.Infrastructure/Contexts/AdoteUmPetDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoteUmPet.Infrastructure.Contexts
+{
+    public class AdoteUmPetDbContext : DbContext
+    {
+        private readonly string _connectionString;
+        public AdoteUmPetDbContext(DbContextOptions options, IConfiguration configuration) : base(options)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            if (string.IsNullOrWhiteSpace(_connectionString))
+                throw new ArgumentNullException("ConnectionString to communicate with Database cannot be found. See your ENV variables.");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseMySql(_connectionString, ServerVersion.AutoDetect(_connectionString))
+                .UseSnakeCaseNamingConvention();
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
