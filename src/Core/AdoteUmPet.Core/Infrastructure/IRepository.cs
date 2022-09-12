540ba7c20cf8332d5668f9ff55e7f8b0826e35fe
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-09-12 14:34:48 -0300

    Criação da abstração de Entidade e regras de Domínio

---
+++ src/Core/AdoteUmPet.Core/Infrastructure/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using AdoteUmPet.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoteUmPet.Core.Infrastructure
+{
+    public interface IRepository<TEntity> where TEntity : Entity, IAggregateRoot
+    {
+
+    }
+}
