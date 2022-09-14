8cc1e369527026ba3d4b9b6baa5debaa4a0f218c
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-09-13 23:41:46 -0300

    Criação de Entidade Pet

---
+++ src/Services/AdoteUmPet/AdoteUmPet.Domain/Pets/Pet.cs
@@ -0,0 +1,15 @@
+﻿using AdoteUmPet.Core.Domain;
+using AdoteUmPet.Core.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoteUmPet.Domain.Pets
+{
+    public class Pet : Entity, IAggregateRoot
+    {
+
+    }
+}
