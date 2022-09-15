cecb99e74729fc3372803fa9efab83813d5b98ac
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-09-14 23:26:05 -0300

    Criação da entidade Favorites

---
+++ src/Services/AdoteUmPet/AdoteUmPet.Domain/Favorites/Enums/FavoriteTypesEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoteUmPet.Domain.Favorites.Enums
+{
+    public enum FavoriteTypesEnum
+    {
+        Ad = 1,
+        Pet = 2
+    }
+}
---
+++ src/Services/AdoteUmPet/AdoteUmPet.Domain/Favorites/Favorite.cs
@@ -0,0 +1,25 @@
+﻿using AdoteUmPet.Core.Domain;
+using AdoteUmPet.Domain.Favorites.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoteUmPet.Domain.Favorites
+{
+    public class Favorite : Entity
+    {
+        public int UserId { get; private set; }
+        public int OptionId { get; private set; }
+        public FavoriteTypesEnum OptionType { get; private set; }
+
+        protected Favorite() { }
+        public Favorite(int userId, int optionId, FavoriteTypesEnum optionType)
+        {
+            UserId = userId;
+            OptionId = optionId;
+            OptionType = optionType;
+        }
+    }
+}
