5335015ae9aa293dd5b6e9f969a354af362807e0
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-09-14 23:26:29 -0300

    Criação do relacionamento de User com Ads

--- src/Services/AdoteUmPet/AdoteUmPet.Domain/Users/User.cs
+++ src/Services/AdoteUmPet/AdoteUmPet.Domain/Users/User.cs
@@ -1,4 +1,7 @@
 ﻿using AdoteUmPet.Core.Infrastructure;
+using AdoteUmPet.Domain.Ads;
+using AdoteUmPet.Domain.Favorites;
+using AdoteUmPet.Domain.Pets;
 using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
@@ -11,6 +14,9 @@ namespace AdoteUmPet.Domain.Users
     public class User: IdentityUser<int>, IAggregateRoot
     {
         public string Name { get; private set; }
+        public ICollection<Ad> Ads { get; private set; }
+        public ICollection<Pet> Pets { get; private set; }
+        public ICollection<Favorite> Favorites { get; private set; }
 
         public User(string name, string email)
         {
@@ -18,6 +24,48 @@ namespace AdoteUmPet.Domain.Users
             UserName = email;
             Email = email;
             EmailConfirmed = true;
+        }
+
+        public void ChangeFavorite(Favorite newFavorite)
+        {
+            if (newFavorite == null)
+                throw new Exception("Favorite cannot be null");
+
+            Favorite favorite = Favorites.FirstOrDefault(f => 
+                f.UserId == newFavorite.UserId && 
+                f.OptionId == newFavorite.OptionId &&           
+                f.OptionType == newFavorite.OptionType);
+
+            if (favorite == null)
+            {
+                Favorites.Add(newFavorite);
+            }
+            else
+            {
+                Favorites.Remove(favorite);
+            }
+        }
+
+        public void AddNewAd(Ad ad)
+        {
+            if (ad == null)
+                throw new Exception("Ad cannot be null");
+
+            if (Ads.Any(a => a.Id == ad.Id))
+                return;
+
+            Ads.Add(ad);
+        }
+
+        public void AddNewPet(Pet pet)
+        {
+            if (pet == null)
+                throw new Exception("Pet cannot be null");
+
+            if (Pets.Any(p => p.Id == pet.Id))
+                return;
+
+            Pets.Add(pet);
         }
     }
 }
