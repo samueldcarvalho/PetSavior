0935d21ab4bfbd343c3fba6a04481b59423f6840
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-09-14 21:04:54 -0300

    Modelagem da entidade de Pet  e classes agregadas.

---
+++ src/Services/AdoteUmPet/AdoteUmPet.Domain/Pets/PetBreed.cs
@@ -0,0 +1,21 @@
+﻿using AdoteUmPet.Core.Domain;
+
+namespace AdoteUmPet.Domain.Pets
+{
+    public class PetBreed : Entity
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string Group { get; private set; }
+        public string ArticleURL { get; private set; }
+
+        protected PetBreed() { }
+        public PetBreed(string name, string description, string group, string articleURL = "")
+        {
+            Name = name;
+            Description = description;
+            ArticleURL = articleURL;
+            Group = group;
+        }
+    }
+}
---
+++ src/Services/AdoteUmPet/AdoteUmPet.Domain/Pets/PetTemperament.cs
@@ -0,0 +1,27 @@
+﻿using AdoteUmPet.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoteUmPet.Domain.Pets
+{
+    public class PetTemperament : Entity
+    {
+        public string TemperamentName { get; private set; }
+        public int TemperamentLevel { get; private set; }
+
+        protected PetTemperament() { }
+        public PetTemperament(string temperamentName, int temperamentLevel)
+        {
+            TemperamentName = temperamentName;
+            TemperamentLevel = temperamentLevel;
+        }
+
+        public void ChangeTemperamentLevel(int level)
+        {
+            TemperamentLevel = level;
+        }
+    }
+}
