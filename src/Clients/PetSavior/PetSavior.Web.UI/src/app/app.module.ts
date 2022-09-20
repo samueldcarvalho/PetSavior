6a609020aa32076361654057c3cf128bb6979d9e
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2022-09-20 20:53:11 -0300

    Criação de components de PetCard

--- src/Clients/PetSavior/PetSavior.Web.UI/src/app/app.module.ts
+++ src/Clients/PetSavior/PetSavior.Web.UI/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PetCardComponent } from './components/pets/pet-card/pet-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    PetCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/Clients/PetSavior/PetSavior.Web.UI/src/app/components/pets/pet-card/pet-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-card',
+  templateUrl: './pet-card.component.html',
+  styleUrls: ['./pet-card.component.css']
+})
+export class PetCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
