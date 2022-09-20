31fedcc6e76d3dd40c0b08f137907dfafd9f7920
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-09-19 21:30:32 -0300

    Estilização da tela principal

--- src/Clients/PetSavior/PetSavior.Web.UI/src/app/app.module.ts
+++ src/Clients/PetSavior/PetSavior.Web.UI/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
