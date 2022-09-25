2472da0e7c483e255b77e6905d6a7d8226b120f9
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2022-09-25 10:00:23 -0300

    Criação do Modulo de Autenticação e componentes de Register e Login

--- src/Clients/PetSavior/PetSavior.Web.UI/src/app/app.module.ts
+++ src/Clients/PetSavior/PetSavior.Web.UI/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavigationModule } from './navigation/navigation.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, NavigationModule],
+  imports: [BrowserModule, HttpClientModule, NavigationModule, AppRoutingModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
