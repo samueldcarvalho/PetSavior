d72c5015ff7c3100185763660b4840678c4769bc
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2022-09-25 11:10:20 -0300

    Finalização do NavigationSystem

--- src/Clients/PetSavior/PetSavior.Web.UI/src/app/app-routing.module.ts
+++ src/Clients/PetSavior/PetSavior.Web.UI/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
+import { NotFoundComponent } from './navigation/not-found/not-found.component';
+import { AccountModule } from './account/account.module';
 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './navigation/home/home.component';
 
 const routes: Routes = [
-  {path: "", redirectTo: "/home", pathMatch: "full"},
-  {path: "home", component: HomeComponent}
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'account',
+    loadChildren: () =>
+      import('./account/account.module').then((res) => res.AccountModule),
+  },
+  {path: "**", component: NotFoundComponent}
 ];
 
 @NgModule({
--- src/Clients/PetSavior/PetSavior.Web.UI/src/app/navigation/navigation.module.ts
+++ src/Clients/PetSavior/PetSavior.Web.UI/src/app/navigation/navigation.module.ts
@@ -5,9 +5,10 @@ import { PetCardComponent } from '../components/pets/pet-card/pet-card.component
 import { PetService } from '../services/pets/pet.service';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [PetCardComponent, HomeComponent, HeaderComponent],
+  declarations: [PetCardComponent, HomeComponent, HeaderComponent, NotFoundComponent],
   imports: [CommonModule, RouterModule],
   providers: [PetService],
   exports: [PetCardComponent, HomeComponent, HeaderComponent],
