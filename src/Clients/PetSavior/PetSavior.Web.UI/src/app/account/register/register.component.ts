53a57cc99cc2061ba309820c374780812547f1d9
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2022-09-27 23:44:05 -0300

    Estilização da tela de Register

--- src/Clients/PetSavior/PetSavior.Web.UI/src/app/account/register/register.component.ts
+++ src/Clients/PetSavior/PetSavior.Web.UI/src/app/account/register/register.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['../login/login.component.css']
 })
 export class RegisterComponent implements OnInit {
 
