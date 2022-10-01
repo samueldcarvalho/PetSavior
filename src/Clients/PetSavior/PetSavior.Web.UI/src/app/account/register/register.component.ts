5805842f0d84a5b5ba6acf2ce8e4b0aab6332c7f
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2022-10-01 11:09:06 -0300

    Validators RegisterForm

--- src/Clients/PetSavior/PetSavior.Web.UI/src/app/account/register/register.component.ts
+++ src/Clients/PetSavior/PetSavior.Web.UI/src/app/account/register/register.component.ts
@@ -1,9 +1,7 @@
 import { AccountService } from './../services/account.service';
 import { NewUserDTO } from './../../models/users/DTOs/new-user.model';
-import { FormGroup, FormBuilder, Validators, FormControl, ValidatorFn, FormControlName } from '@angular/forms';
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChildren } from '@angular/core';
-import { DisplayMessage, GenericFormValidator, ValidationMessages } from 'src/app/utils/generic-form-validation';
-import { fromEvent, merge, Observable } from 'rxjs';
+import { FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -20,38 +18,43 @@ export class RegisterComponent implements OnInit, AfterViewInit {
   ) {}
 
   ngOnInit(): void {
-    this.registerForm = this._formBuilder.group({
-      name: ['', [Validators.required, Validators.minLength(4)]],
-      email: ['', [Validators.required, Validators.email]],
-      password: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(6),
-          Validators.maxLength(15),
+    this.registerForm = this._formBuilder.group(
+      {
+        name: ['', [Validators.required, Validators.minLength(4)]],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(15),
+          ],
         ],
-      ],
-      repeatPassword: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(6),
-          Validators.maxLength(15),
-        ],
-      ],
-    });
+        repeatPassword: ['', [Validators.required]],
+      }
+    );
+  }
+
+  ngAfterViewInit(): void {}
+
+  registerUser() {
+    if (
+      (this.repeatPassword().value != this.password().value &&
+      this.password().dirty &&
+      this.repeatPassword().dirty) ||
+      !this.registerForm.valid
+    ){
+      return;
+    }
+
+
+      console.log(this.registerForm.value);
+
+    this.newUser = Object.assign({}, this.newUser, this.registerForm.value);
   }
 
   name = () => this.registerForm.get('name')!;
   email = () => this.registerForm.get('email')!;
   password = () => this.registerForm.get('password')!;
   repeatPassword = () => this.registerForm.get('repeatPassword')!;
-
-  ngAfterViewInit(): void {}
-
-  registerUser() {
-    console.log(this.registerForm.value);
-
-    this.newUser = Object.assign({}, this.newUser, this.registerForm.value);
-  }
 }
