4d62573a409def1492ada09b09cc48131da5916c
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2022-10-01 18:33:36 -0300

    Criação e organização de pastas de ViewModels no backend

---
+++ src/Services/PetSavior/PetSavior.Application/Models/ViewModels/Users/UserToken.cs
@@ -0,0 +1,16 @@
+﻿namespace PetSavior.Application.Models.ViewModels.Users
+{
+    public class UserToken
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+
+        public UserToken(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }
+    }
+}
